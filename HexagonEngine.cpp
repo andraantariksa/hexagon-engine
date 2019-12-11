b356ebd3e7a6bea7065d459b484e3af3ca01d23f
Andra Antariksa <andra.antariksa@gmail.com>
2019-12-11 19:52:19 +0700

    Adding Google Test

--- HexagonEngine.cpp
+++ HexagonEngine.cpp
@@ -1,9 +1,9 @@
-//#include "Window/lib.cpp"
-
 #include "Source/Engine/Window/Window.h"
 #include "Dependencies/SDL2/include/SDL.h"
+#include <cstdio>
 
 int main(int argc, char* argv[]) {
+	std::printf("Starting the Hexagon Engine");
 	Hx::Window::Window win = Hx::Window::Window::Builder()
 		.SetSize(1280, 720)
 		.Build();
